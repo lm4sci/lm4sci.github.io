6e273940020274796734fc24bc5b162d7137867a
sepo-agent <279869237+sepo-agent@users.noreply.github.com>
2026-05-20 18:02:19 +0000

    Add Previous Editions section to docs sidebar with 2025 link

--- src/content/2026/_meta.js
+++ src/content/2026/_meta.js
@@ -4,4 +4,12 @@ export default {
     'call-for-papers': { title: 'Call for Papers' },
     schedule: { title: 'Schedule' },
     faq: { title: 'FAQ' },
+    '-- previous-editions': {
+        type: 'separator',
+        title: 'Previous Editions'
+    },
+    '2025-edition': {
+        title: 'LM4Sci 2025',
+        href: '/docs/2025'
+    }
 }
